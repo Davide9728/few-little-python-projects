e1e9fbfe4df78bd270f867a8a929b4b44586a57f
Davide9728 <117565421+Davide9728@users.noreply.github.com>
2022-12-07 12:29:58 +0100

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,5 +1,5 @@
 import random
-number_selected = random.randint(0,100)
+selected_number = random.randint(0, 100)
 flag = False #if number_selected is found, it sto the game
 life = 0
 
@@ -11,22 +11,23 @@ else:
     life = 10
 
 while flag != True:
+    print(f"\nyou have {life} attempts to guess the number")
 
     if life == 0:
         flag = True
-        print("you lost all life\ngame over")
+        print(f"you lost all life\ngame over\nthe answer was {selected_number}")
 
     else:
         player_num = int(input("guess the number"))
-        if player_num == number_selected:
-            print("win")
+        if player_num == selected_number:
+            print(f"you got it, the answer was {selected_number}")
             flag = True
-        elif player_num < number_selected:
+        elif player_num < selected_number:
             print("too low")
             life -= 1
-        elif player_num > number_selected:
+        elif player_num > selected_number:
             print("too high")
             life -= 1
 
 
-print(f"numero random {number_selected}")
+
